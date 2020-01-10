1b7bbc5caa00359694e1ad53907b622878b81a11
Tarun <tarunreddy1018@gmail.com>
2020-01-11 01:41:12 +0530

    Basic template for hibernate + models + REST controllers

---
+++ src/main/java/com/psych/game/model/Auditable.java
@@ -0,0 +1,31 @@
+package com.psych.game.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value={"createdDate", "updatedDate"}, allowGetters = true)
+public class Auditable implements Serializable {
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    @Temporal(TemporalType.DATE)
+    @Getter
+    @Setter
+    private Date createdDate = new Date();
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    @Temporal(TemporalType.DATE)
+    @Getter
+    @Setter
+    private Date updatedAt = new Date();
+}
